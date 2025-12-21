ac2388ba54163aac69160c75eeb53a24d005f889
321error <56364583+321error@users.noreply.github.com>
2025-12-21 17:55:20 +0530

    Update pif.sh

--- pif.sh
+++ pif.sh
@@ -4,18 +4,26 @@
 echo "Crawling Android Developers for latest Pixel Beta ..."
 
 wget -q -O PIXEL_VERSIONS_HTML --no-check-certificate https://developer.android.com/about/versions 2>&1 || exit 1;
-wget -q -O PIXEL_LATEST_HTML --no-check-certificate $(grep -o 'https://developer.android.com/about/versions/.*[0-9]"' PIXEL_VERSIONS_HTML | sort -ru | cut -d\" -f1 | head -n1 | tail -n1) 2>&1 || exit 1;
-wget -q -O PIXEL_OTA_HTML --no-check-certificate https://developer.android.com$(grep -o 'href=".*download-ota.*"' PIXEL_LATEST_HTML | grep 'qpr' | cut -d\" -f2 | head -n1 | tail -n1) 2>&1 || exit 1;
-echo "$(grep -m1 -oE 'tooltip>Android .*[0-9]' PIXEL_OTA_HTML | cut -d\> -f2) $(grep -oE 'tooltip>QPR.* Beta' PIXEL_OTA_HTML | cut -d\> -f2 | head -n1 | tail -n1)";
+wget -q -O PIXEL_LATEST_HTML --no-check-certificate $(grep -o 'https://developer.android.com/about/versions/.*[0-9]"' PIXEL_VERSIONS_HTML | sort -ru | cut -d\" -f1 | head -n$FORCE_TOP | tail -n1) 2>&1 || exit 1;
+wget -q -O PIXEL_OTA_HTML --no-check-certificate https://developer.android.com$(grep -o 'href=".*download-ota.*"' PIXEL_LATEST_HTML | grep 'qpr' | cut -d\" -f2 | head -n$FORCE_DEPTH | tail -n1) 2>&1 || exit 1;
+echo "$(grep -m1 -oE 'tooltip>Android .*[0-9]' PIXEL_OTA_HTML | cut -d\> -f2) $(grep -oE 'tooltip>QPR.* Beta' PIXEL_OTA_HTML | cut -d\> -f2 | head -n$FORCE_DEPTH | tail -n1)";
 
-BETA_REL_DATE="$(date -D '%B %e, %Y' -d "$(grep -m1 -A1 'Release date' PIXEL_OTA_HTML | tail -n1 | sed 's;.*<td>\(.*\)</td>.*;\1;')" '+%Y-%m-%d')";
+if grep -q 'Release date' PIXEL_OTA_HTML; then
+  LONG_REL_DATE="$(grep -m1 -A1 'Release date' PIXEL_OTA_HTML)";
+else
+  wget -q -O PIXEL_FI_HTML --no-check-certificate https://developer.android.com$(grep -o 'href=".*download.*"' PIXEL_LATEST_HTML | grep 'qpr' | cut -d\" -f2 | head -n$FORCE_DEPTH | tail -n1) 2>&1 || exit 1;
+  LONG_REL_DATE="$(grep -m1 -A1 'Release date' PIXEL_FI_HTML)";
+fi;
+
+BETA_REL_DATE="$(date -D '%B %e, %Y' -d "$(echo $LONG_REL_DATE | tail -n1 | sed 's;.*<td>\(.*\)</td>.*;\1;')" '+%Y-%m-%d')";
 BETA_EXP_DATE="$(date -D '%s' -d "$(($(date -D '%Y-%m-%d' -d "$BETA_REL_DATE" '+%s') + 60 * 60 * 24 * 7 * 6))" '+%Y-%m-%d')";
 echo "Beta Released: $BETA_REL_DATE \
   \nEstimated Expiry: $BETA_EXP_DATE";
 
 MODEL_LIST="$(grep -A1 'tr id=' PIXEL_OTA_HTML | grep 'td' | sed 's;.*<td>\(.*\)</td>;\1;')";
-PRODUCT_LIST="$(grep -o 'ota/.*_beta' PIXEL_OTA_HTML | cut -d\/ -f2)";
-OTA_LIST="$(grep 'ota/.*_beta' PIXEL_OTA_HTML | cut -d\" -f2)";
+PRODUCT_LIST="$(grep 'tr id=' PIXEL_OTA_HTML | sed 's;.*<tr id="\(.*\)">;\1_beta;')";
+OTA_LIST="$(grep -o '>.*_beta.*</button' PIXEL_OTA_HTML | sed 's;.*>\(.*\)</button;\1;')";
+OTA_PREFIX="$(grep -m1 'ota/.*_beta' PIXEL_OTA_HTML | cut -d\" -f2 | sed 's;\(.*\)/.*;\1;')";
 
 if [ "$FORCE_MATCH" ]; then
   DEVICE="$(getprop ro.product.device)";
@@ -23,7 +31,7 @@ if [ "$FORCE_MATCH" ]; then
     *" ${DEVICE}_beta "*)
       MODEL="$(getprop ro.product.model)";
       PRODUCT="${DEVICE}_beta";
-      OTA="$(echo "$OTA_LIST" | grep "$PRODUCT")";
+      OTA="$OTA_PREFIX/$(echo "$OTA_LIST" | grep "$PRODUCT")";
     ;;
   esac;
 fi;
@@ -38,7 +46,7 @@ if [ -z "$PRODUCT" ]; then
     set -- $PRODUCT_LIST;
     PRODUCT="$(eval echo \${$list_rand})";
     set -- $OTA_LIST;
-    OTA="$(eval echo \${$list_rand})";
+    OTA="$OTA_PREFIX/$(eval echo \${$list_rand})";
     DEVICE="$(echo "$PRODUCT" | sed 's/_beta//')";
   }
   set_random_beta;
